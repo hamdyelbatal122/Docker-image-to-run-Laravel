e8b6f217cafbd4233facaf48c7d267f6bc2c85d5
Hamdy Elbatal <elbatalhamdy77@gmail.com>
2020-11-23 15:23:56 +0200

    Update install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 mkdir -p /usr/local/lib/nodejs
-
 # Install latest LTS node version
 echo "@edge http://nl.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories
 apk add -U nodejs@edge libuv@edge
